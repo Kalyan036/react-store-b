c889d504f2fdb2bc64cc58d29f9dc0b4554c18a0
Kalyan036 <padamkalyan80@gmail.com>
2025-04-04 10:03:32 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,27 +7,32 @@ import Products from "./components/Products";
 import {useState} from "react"
 import {createContext} from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Orders from "./components/Orders";
 export const appContext = createContext();
 function App(props) {
   const [users, setUsers] = useState([]);
   const [user, setUser] = useState({});
   const [cart,setCart] = useState({});
+  const [orders,setOrders] = useState([]);
   const products = [
-    { id: 1, name: "Product 1", price: 56 },
-    { id: 2, name: "Product 2", price: 40 },
-    { id: 3, name: "Product 3", price: 35 },
-    { id: 4, name: "Product 4", price: 25 },
-    { id: 5, name: "Product 5", price: 95 },
-    { id: 6, name: "Product 6", price: 85 },
+    { id: 1, name: "Product 1", price: 56,url:"https://picsum.photos/id/0/300/300" },
+    { id: 2, name: "Product 2", price: 40,url:"https://picsum.photos/id/1/300/300" },
+    { id: 3, name: "Product 3", price: 35,url:"https://picsum.photos/id/2/300/300" },
+    { id: 4, name: "Product 4", price: 25,url:"https://picsum.photos/id/3/300/300" },
+    { id: 5, name: "Product 5", price: 95,url:"https://picsum.photos/id/4/300/300" },
+    { id: 6, name: "Product 6", price: 85,url:"https://picsum.photos/id/5/300/300" },
   ];
+  const [placeOrders, setPlaceOrders] = useState([]);
   return (
     <BrowserRouter>
-    <appContext.Provider value={{users,setUsers,user,setUser,cart,setCart,products}}>
+    <appContext.Provider 
+    value={{users,setUsers,user,setUser,cart,setCart,products,placeOrders, setPlaceOrders, orders, setOrders}}>
       <Header />
       <Routes>
         <Route index element={<Products />} />
         <Route path="home" element={<Products />} />
         <Route path="cart" element={<Cart />} />
+        <Route path="orders" element={<Orders />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
       </Routes>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,19 +4,41 @@ import { appContext } from "../App";
 import { useContext } from "react";
 export default function Products() {
   const { user,cart,setCart,products } = useContext(appContext);
+  const addToCart = (id) => {
+    setCart({...cart,[id]: 1})    
+  }
+
+  const increment = (id) => {
+    setCart({ ...cart, [id]: cart[id] + 1 });
+  };
+
+  const decrement = (id) => {
+    setCart({ ...cart, [id]: cart[id] - 1 });
+  };
   return(
   <>
-    <h3>Hello {user.name}</h3>
+  <h3>{user.name}</h3>
     <div className="App-Products-Row">
       {products.map((value, index) => (
         <div key={index} className="App-Products-Box">
+          <img src={value.url}></img>
           <h3>{value.name}</h3>
           <h4>{value.price}</h4>
-          <button>Add to Cart</button>
+          {cart[value.id] > 0 ? (
+            <div>
+              <button onClick={() => decrement(value.id)}>-</button>
+              {cart[value.id]}
+              <button onClick={() => increment(value.id)}>+</button>
+            </div>
+          ) : (
+            <div>
+              <button onClick={() => addToCart(value.id)}>Add to cart</button>
+            </div>
+          )
+          }
         </div>
       ))}
     </div>
-    ;
   </>
   )
 }
